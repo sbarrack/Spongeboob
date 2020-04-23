2cc69fb5afdd6003edb58c69dc4650b0837f6e36
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-04-22 22:06:49 -0700

    hello world

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('config.json'));
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(config.token);
