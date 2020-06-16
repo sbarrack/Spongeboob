a652bef69a218050c96328b5d325cd68e072a61a
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-06-15 21:19:47 -0700

    copy site template

---
+++ docs/js/script.js
@@ -0,0 +1,11 @@
+(function ($, AOS) {
+    $(document).ready(function () {
+        AOS.init(/* {
+            offset: 200,
+            duration: 600,
+            easing: 'ease-in-sine',
+            delay: 100,
+        } */);
+
+    });
+})($, AOS);
