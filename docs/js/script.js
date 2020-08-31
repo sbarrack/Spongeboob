945b2133dea689791e4fb3c6ac1e1812889c9552
sbarrack <12619603+sbarrack@users.noreply.github.com>
2020-08-31 09:47:25 -0700

    back to rotating bg img

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,3 @@
-(function (Pano) {
-    const panorama = new Pano.ImagePanorama( '../images/panorama2.jpg' );
-    const viewer = new Pano.Viewer();
-    viewer.add( panorama );
-})(PANOLENS);
+(function () {
+
+})();
