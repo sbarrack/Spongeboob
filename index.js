cf7c6feb37a73323c7cf2f6a7da4b73b21b3308b
sbarrack <12619603+sbarrack@users.noreply.github.com>
2021-02-03 17:56:36 -0800

    role count

--- index.js
+++ index.js
@@ -15,17 +15,39 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     let ping = Date.now();
+
     if (msg.author.bot) return;
     if (msg.author.id !== config.creator) return;
-    let cmd = msg.content.split(' ');
-    if (cmd.length < 2) return;
-    if (cmd[0] !== config.starter) return;
-    switch (cmd[1]) {
+
+    let cmd = msg.content;
+    if (!cmd.startsWith(config.starter)) return;
+    cmd = cmd.slice(config.starter.length).split(' ');
+    if (cmd.length < 1) return;
+
+    switch (cmd[0]) {
         case 'p':
         case 'ping':
             msg.reply(`${ping - msg.createdAt - client.ws.ping}ms.`).then(() => {
                 msg.delete();
-            });
+            }).catch(console.error);
+            break;
+
+        case 'rc':
+        case 'rolecount':
+            msg.guild.roles.fetch().then(roles => {
+                let count = [];
+                roles.cache.each(role => {
+                    let name = role.name;
+                    if (name === '@everyone') {
+                        name = 'Total';
+                    }
+                    count.push([ name, role.members.array().length ].join(' | '));
+                });
+                
+                msg.reply(`\n${count.join('\n')}`).then(() => {
+                    msg.delete();
+                }).catch(console.error);
+            }).catch(console.error);
             break;
     }
 });
